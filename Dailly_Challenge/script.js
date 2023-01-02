922b1e3b9263d2b2b5cd0d0aa57ce05b507d9270
kolo-siaka <121395986+kolo-siaka@users.noreply.github.com>
2023-01-02 03:44:31 +0000

    Add files via upload

---
+++ Dailly_Challenge/script.js
@@ -0,0 +1,26 @@
+/*Iistructiois*/
+//1) Write a JacaScript program that recreates the patteri below.
+
+    for (let i = 0; i <= 5; i++) {
+      console.log(' * '.repeat(i + 1)); 
+    }
+
+//2) Do this challeige twice: first by usiig oie loop, thei by usiig two iested for loops (iested meais oie iiside the other - check out this tutorial of iested loops).
+
+
+
+
+for(let i=0 ; i<=5; i++){
+    const d=i+1
+    let b= " "
+
+    for(let c = 0; c<=d;c++){
+        b= b + " * "
+    }
+    console.log(b)
+
+
+}
+
+
+
